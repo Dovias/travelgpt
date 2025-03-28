54a61dd88635b8136b0cdf5d8b4e605e4c8c355e
Dovias <70913423+Dovias@users.noreply.github.com>
2025-03-29 00:00:49 +0200

    TGPT-35: Add initial project server files

---
+++ server/Controllers/Root.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+public class Root : Controller {
+    [HttpGet("")]
+    public IActionResult Get() {
+        return Ok("Hello world from travelgpt server!");
+    }
+}
