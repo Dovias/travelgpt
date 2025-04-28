d96a4505c81e3a7b2ba3d29b81aa058e76fd74bb
Aleks Aleks <aleksandr.bunejev@stud.vilniustech.lt>
2025-04-29 01:25:31 +0300

    TGPT-33, TGPT-27

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,9 @@ import {
   SentChatMessageResponse,
 } from "./types/chatTypes";
 import fetchMessage from "./functions/fetchMessage";
+import DarkModeToggle from "./components/DarkModeToggle";
+import ExportChatButton from "./components/ExportChatButton";
+import NewChatButton from "./components/NewChatButton";
 
 function App() {
   const [messages, setMessages] = useState<string[]>([]);
@@ -31,38 +34,63 @@ function App() {
     return data.id;
   };
 
+  const initChat = async () => {
+    const tempChatId = await createChat();
+    const response = await fetchMessage(
+      "We're starting a conversation. Greet me ONLY THIS TIME AND NEVER AGAIN depending on the time of day it is, current time: " +
+        new Date().getHours() +
+        " hours.",
+      tempChatId
+    );
+    const data = await response.json();
+    const newMessage = (data as SentChatMessageResponse).text;
+    setMessages(prev => [...prev, newMessage]);
+  };
+
   useEffect(() => {
-    const initChat = async () => {
-      const tempChatId = await createChat();
-      const response = await fetchMessage(
-        "We're starting a conversation. Greet me ONLY THIS TIME AND NEVER AGAIN depending on the time of day it is, current time: " +
-          new Date().getHours() +
-          " hours.",
-        tempChatId
-      );
-      setMessages([
-        ...messages,
-        ((await response.json()) as SentChatMessageResponse).text,
-      ]);
-    };
+    // const initChat = async () => {
+    //   const tempChatId = await createChat();
+    //   const response = await fetchMessage(
+    //     "We're starting a conversation. Greet me ONLY THIS TIME AND NEVER AGAIN depending on the time of day it is, current time: " +
+    //       new Date().getHours() +
+    //       " hours.",
+    //     tempChatId
+    //   );
+    //   setMessages([
+    //     ...messages,
+    //     ((await response.json()) as SentChatMessageResponse).text,
+    //   ]);
+    // };
 
     initChat();
   }, []);
 
+  const createNewChat = () => {
+    setMessages([]);
+    setChatId(undefined);
+    initChat()
+  }
+
   return (
-    <div className="scheme-dark flex flex-col gap-4 w-screen h-screen overflow-auto bg-neutral-800 text-white">
+    <div className="scheme-light dark:scheme-dark flex flex-col gap-4 w-screen h-screen overflow-auto bg-neutral-800 text-white relative">
+      <div className="absolute top-4 right-4 flex flex-col gap-2">
+        <NewChatButton onNewChat={createNewChat}></NewChatButton>
+        <ExportChatButton messages={messages} chatId={chatId}></ExportChatButton>
+        {/* <DarkModeToggle></DarkModeToggle> */}
+      </div>
       <div className="flex-1 overflow-auto">
         <div className="flex flex-col h-full overflow-auto max-w-5xl mx-auto relative">
           <div className="text-center m-16">
             <h1 className="m-1 text-5xl font-bold">Gemini.NET 🗺️</h1>
             <p className="m-1">Gemini LLM wrapper in React and ASP.NET!</p>
           </div>
-          <MessageList messages={messages}></MessageList>
-          <Input onSendMessage={handleSendMessage}></Input>
+          <MessageList messages={messages} />
+          <Input onSendMessage={handleSendMessage} />
         </div>
       </div>
     </div>
   );
+  
 }
 
 export default App;
