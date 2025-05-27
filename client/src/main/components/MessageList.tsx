ed193fdffcb32957cadcfd35f94c65d507c7dc92
Rokas Bloznelis <rokas.bloznelis@gmail.com>
2025-05-27 19:36:19 +0300

    Realising feat/TGPT-19 - formatting Chatbot messages to have headings, names of places in bold

--- client/src/main/components/MessageList.tsx
+++ client/src/main/components/MessageList.tsx
@@ -1,5 +1,24 @@
+import { JSX } from "react";
+
 interface Props {
   messages: string[];
+}
+
+function parseMessage(message: string): JSX.Element[] {
+  return message.split("\n").map((line, idx) => {
+    const parts = line.split(/(\*\*[^*]+\*\*)/g).map((part, i) => {
+      if (/^\*\*[^*]+\*\*$/.test(part)) {
+        return <strong key={i}>{part.replace(/^\*\*|\*\*$/g, "")}</strong>;
+      }
+      return <span key={i}>{part}</span>;
+    });
+
+    return (
+      <p key={idx}>
+        {parts}
+      </p>
+    );
+  });
 }
 
 function MessageList({ messages }: Props) {
@@ -10,10 +29,8 @@ function MessageList({ messages }: Props) {
           key={index}
           className={`w-full even:text-right even:[&>*]:bg-blue-500 odd:[&>*]:bg-gray-700`}
         >
-          <div
-            className={`inline-block max-w-lg p-3 rounded-xl text-white text-left`}
-          >
-            <p className="whitespace-pre-wrap break-words">{message}</p>
+          <div className="inline-block max-w-lg p-3 rounded-xl text-white text-left whitespace-pre-wrap break-words">
+            {parseMessage(message)}
           </div>
         </div>
       ))}
